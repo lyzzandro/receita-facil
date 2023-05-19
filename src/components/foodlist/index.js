e43c928841d29068387353254acdd9366c93bb59
Your Name <you@example.com>
2023-05-19 18:32:45 -0300

    add pages

---
+++ src/components/foodlist/index.js
@@ -0,0 +1,8 @@
+import { View, Text, StyleSheet } from 'react-native';
+export function FoodList() {
+    return (
+        <View>
+            <Text>olawdaw</Text>
+        </View>
+    );
+}
---
+++ src/pages/favorites/index.js
@@ -0,0 +1,17 @@
+import { View, Text, StyleSheet } from 'react-native';
+
+export function Favorites() {
+    return (
+        <View style={styles.container}>
+            <Text>Página de favoritos</Text>
+            <Text>Página de favoritos</Text>
+            <Text>Página de favoritos</Text>
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({
+    container: {
+        backgroundColor: 'yellow',
+    },
+});
